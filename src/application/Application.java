e09225e0e15a3ac41c8811553ab0dc4999db8424
liamandaidan <liammclaughlinis@gmail.com>
2021-05-28 13:41:03 -0600

    Initial Driver Class Methods and More
    Team effort

--- src/application/Application.java
+++ src/application/Application.java
@@ -5,11 +5,9 @@ import managers.Driver;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-
-		System.out.println("Ben wrote this");
-		System.out.println("This is Liam");
+		
+		Driver driver = new Driver();
+		
 	}
 
 }
--- src/managers/Driver.java
+++ src/managers/Driver.java
@@ -1,7 +1,91 @@
 package managers;
 
-import problemdomain.*; 
+import problemdomain.*;
+import java.io.*;
+import java.util.*;
 
+/**
+ * 
+ * @author Liam, Robyn, Benson, Mike
+ * @version 1.0
+ * 
+ */
 public class Driver {
+
+	private final String FILEPATH = "res/books.txt";
+	private ArrayList<Book> bookList;
+
+	/**
+	 * 
+	 */
+	public Driver() {
+		bookList = new ArrayList<Book>();
+		loadBooks();
+		displayMenu();
+	}
+
+	/**
+	 * 
+	 */
+	public void loadBooks() {
+		try {
+			Scanner load = new Scanner(new File(FILEPATH));
+			System.out.println("Worked");
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Did not read in file properly, restart the program, and check filepath of books.txt.");
+		}
+	}
+
+	/**
+	 * 
+	 */
+	public void displayMenu() {
+		// Todo create menu
+
+		// From menu four functions to call
+		checkOutBook();
+		findBookTitle();
+		displayBookType();
+		produceRandomBookList();
+		// Save book and exit
+		saveBook();
+
+	}
+	/**
+	 * 
+	 */
+	public void checkOutBook() {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * 
+	 */
+	public void findBookTitle() {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * 
+	 */
+	public void displayBookType() {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * 
+	 */
+	public void produceRandomBookList() {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * 
+	 */
+	public void saveBook() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
