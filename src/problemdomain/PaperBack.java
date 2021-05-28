5dc87337d19503c36883fb2d636397200443b369
bdavi <b.davis_117@hotmail.ca>
2021-05-28 12:21:45 -0600

    paperback changes

--- src/problemdomain/PaperBack.java
+++ src/problemdomain/PaperBack.java
@@ -2,4 +2,53 @@ package problemdomain;
 
 public class PaperBack extends Book {
 	//this is a Ben comment
+	private String authors;
+	private int year;
+	private char genre;
+	
+	
+	/**
+	 * @return the authors
+	 */
+	public String getAuthors()
+	{
+		return authors;
+	}
+	/**
+	 * @return the year
+	 */
+	public int getYear()
+	{
+		return year;
+	}
+	/**
+	 * @return the genre
+	 */
+	public char getGenre()
+	{
+		return genre;
+	}
+	/**
+	 * @param authors the authors to set
+	 */
+	public void setAuthors(String authors)
+	{
+		this.authors = authors;
+	}
+	/**
+	 * @param year the year to set
+	 */
+	public void setYear(int year)
+	{
+		this.year = year;
+	}
+	/**
+	 * @param genre the genre to set
+	 */
+	public void setGenre(char genre)
+	{
+		this.genre = genre;
+	}
+	
+	
 }
