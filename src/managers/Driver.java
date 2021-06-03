8414ce92a311363c89fbc8b5ad897fab532e5759
rawbz <rmjbalanag@gmail.com>
2021-06-02 20:47:41 -0600

    updated javadoc and corrected comment

--- src/managers/Driver.java
+++ src/managers/Driver.java
@@ -361,7 +361,7 @@ public class Driver {
 	 * Method will grab x random books from user and display random books.
 	 * 
 	 * @author Liam
-	 * @param numOfbooks this will take in the number of books from user.
+	 * @param numOfBooks this will take in the number of books from user.
 	 * 
 	 */
 	public void produceRandomBookList(int numOfBooks) {
